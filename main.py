39d874e709d1955a46eeec5bc9dc1b03c0c7fa5b
JUULG-SCHOOLPC\windows <mushroomsando@naver.com>
2023-08-11 23:16:44 +0900

    예제코드 수정

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 """
 In order to inquire about the special report issued in the area based on the special report area code, 
@@ -8,21 +9,61 @@ zone name, special report strength, effective time, release time, total cancella
 Power by Korea Meteorological Administration
 Copyright (C) 2023 Mushroomsando. All right reserved.
 """
+with open('alret_info.json', 'r', encoding='utf-8') as json_file:
+    reference_data = json.load(json_file)
 
-# basics
-key = open("api_key.txt", "r")
+def get_raw_data(service_key, areacode, branchCod, warningType = '', fromTime = '', toTime = ''):
+    url = 'http://apis.data.go.kr/1360000/WthrWrnInfoService/getPwnCd'
+    params ={'serviceKey' : service_key, # Essential, Your API KEY
+            'pageNo' : '1', # Essential, Page Number
+            'numOfRows' : '10', # Essential, Number of results per page
+            'dataType' : 'JSON', # Option, Defalt : XML, Data type (XML or JSON)
+            'fromTmFc' : fromTime, # Option, Time (YYYYMMDD), Current date if not entered
+            'toTmFc' : toTime, # Option, Time (YYYYMMDD), Current date if not entered
+            'areaCode' : areacode, # Option, areaCode
+            'warningType' : warningType, # Option, 1-strong wind, 2-rain, 3-cold wave, 4-dry, 5-storm tidal wave, 6-wind, 7-tropical wind, 8-snow, 9-yellow sand, 12-heat wave
+            'stnId' : branchCod # Essential, Branch code
+            }
+    
+    response = requests.get(url, params=params)
+    data = response.json()
+    
+    if data["response"]["header"]["resultCode"] == "3":
+        raise ValueError("데이터가 없습니다. 파라메터 값을 올바르게 입력했는지 확인해 주세요.")
+    else:
+        return data
 
-url = 'http://apis.data.go.kr/1360000/WthrWrnInfoService/getPwnCd'
-params ={'serviceKey' : key.read(), # Essential, Your API KEY
-         'pageNo' : '1', # Essential, Page Number
-         'numOfRows' : '10', # Essential, Number of results per page
-         'dataType' : 'JSON', # Option, Defalt : XML, Data type (XML or JSON)
-         'fromTmFc' : '', # Option, Time (YYYYMMDD), Current date if not entered
-         'toTmFc' : '', # Option, Time (YYYYMMDD), Current date if not entered
-         'areaCode' : 'L1080100', # Option, areaCode
-         'warningType' : '', # Option, 1-strong wind, 2-rain, 3-cold wave, 4-dry, 5-storm tidal wave, 6-wind, 7-tropical wind, 8-snow, 9-yellow sand, 12-heat wave
-         'stnId' : '108' # Essential, Branch code
-         }
+def extract_weather_info(raw_data):
+    data = dict()
 
-response = requests.get(url, params=params)
-print(response.content)
+    for item in raw_data["response"]["body"]["items"]["item"]:
+        forecast_time = item["tmFc"]
+        area_name = item["areaName"]
+        warn_value = item["warnVar"]
+        warn_stress = item["warnStress"]
+        command = item["command"]
+        start_time = item["startTime"]
+        end_time = item["endTime"]
+        is_cancel = item["cancel"]
+
+        warn_var_name = reference_data["warnVar"][str(warn_value)]
+        warn_stress_name = reference_data["warnStress"][str(warn_stress)]
+        command_name = reference_data["command"][str(command)]
+    
+    data_item = {
+        "forecast_time": forecast_time,
+        "area_name": area_name,
+        "warn_value": warn_var_name,
+        "warn_stress": warn_stress_name,
+        "command_name": command_name, 
+        "start_time": start_time,
+        "end_time": end_time,
+        "is_cancel": is_cancel
+    }
+
+    data[forecast_time] = data_item
+
+    return data
+
+raw = get_raw_data(open("api_key.txt", "r"), "L1080100", "108")
+print(extract_weather_info(raw))
